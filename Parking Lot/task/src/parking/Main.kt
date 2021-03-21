6ad1b2cb37981371548d2ab78645200ce522febc
NikoBolt <docnikbolt@gmail.com>
2021-03-21 23:21:03 +0300

    Stage 2/5: Only 2 spots

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,7 +1,24 @@
 package parking
 
+import java.util.*
+
 fun main() {
-    println("White car has parked.\n" +
-            "Yellow car left the parking lot.\n" +
-            "Green car just parked here.")
+    val scanner = Scanner(System.`in`)
+    val input = scanner.nextLine()
+    val str = input.split(" ")
+
+    val spots: MutableMap<Int, String> = mutableMapOf<Int, String>()
+    spots[1] = " "
+    spots[2] = ""
+
+    when (str[0]) {
+        "park" -> println("${str[str.size - 1]} car parked in spot 2.")
+        "leave" -> {
+            if(spots.get(str[1].toInt()) == "") println("There is no car in spot ${str[1]}.")
+            else {
+                spots[str[1].toInt()] = ""
+                println("Spot 1 is free.")
+            }
+        }
+    }
 }
