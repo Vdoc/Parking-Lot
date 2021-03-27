2a1b1df8e506d0278dc7cabe12e598543ce9617e
NikoBolt <docnikbolt@gmail.com>
2021-03-27 19:08:03 +0300

    Stage 3/5: Expand and park in Parking Lot

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,24 +1,79 @@
 package parking
 
-import java.util.*
+import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val input = scanner.nextLine()
-    val str = input.split(" ")
+    var size = 20
+    val parking = Parking(size)
+    parking.start()
+}
 
+class Parking {
+    var size: Int
     val spots: MutableMap<Int, String> = mutableMapOf<Int, String>()
-    spots[1] = " "
-    spots[2] = ""
+    var exit = false
+    val scanner = Scanner(System.`in`)
+    var input: List<String> = emptyList()
+    var minEmpty: Int = 1
 
-    when (str[0]) {
-        "park" -> println("${str[str.size - 1]} car parked in spot 2.")
-        "leave" -> {
-            if(spots.get(str[1].toInt()) == "") println("There is no car in spot ${str[1]}.")
-            else {
-                spots[str[1].toInt()] = ""
-                println("Spot 1 is free.")
+    constructor (_size: Int) {
+        size = _size
+        initializePark()
+    }
+
+    fun initializePark() {
+        for (i in 1..size) {
+            spots[i] = ""
+        }
+    }
+
+    fun start() {
+        while (!exit) {
+            input = scanner.nextLine().split(" ")
+
+            when (input[0]) {
+                "park" -> park()
+                "leave" -> leave(input[1].toInt())
+                "exit" -> exit = true
+                else -> println("Incorrect input")
             }
         }
     }
+
+    fun park() {
+        if (minEmpty == 0) {
+            println("Sorry, the parking lot is full.")
+            return
+        }
+        spots[minEmpty] = input[2]
+        println("${spots[minEmpty]} car parked in spot ${minEmpty}.")
+        findNextEmpty(minEmpty)
+    }
+
+    fun leave(spot: Int) {
+        if (spots.get(spot) == "")
+            println("There is no car in spot $spot.")
+        else {
+            spots[spot] = ""
+            println("Spot $spot is free.")
+        }
+        minEmptySpot(spot)
+    }
+
+    fun findNextEmpty(_minEmpty: Int) {
+        for (i in _minEmpty + 1..size) {
+            if (spots[i] == "") {
+                minEmpty = i
+                return
+            }
+        }
+        minEmpty = 0
+    }
+
+    private fun minEmptySpot(spot: Int): Int {
+        if (minEmpty > spot || minEmpty == 0)
+            minEmpty = spot
+        return minEmpty
+    }
 }
+
